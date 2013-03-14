6e8f1909d63e9a1ce8dfcb4db5cf76b3a6bdf1a0
marklevi <mdayanlevi@gmail.com>
2013-03-14 17:02:43 -0400

    dev-sprint0

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,22 +1,68 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+Mark Levi
 
 
-from TurtleWorld import * 		
-world = TurtleWorld()			
-bob = Turtle()				
+from TurtleWorld import *
+world = TurtleWorld()
+bob = Turtle()
+
+bob.delay = 0.01
+
+import math
+def polyline(t, n, length, angle):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle)
+
+def polygon(t, n, length):
+	angle = 360.0 / n
+	polyline(t, n, length, angle)
+
+def arc(t, r, angle):
+	arc_length = 2 * math.pi * r * angle / 360
+	n = int(arc_length / 3) + 1
+	step_length = arc_length / n
+	step_angle = float(angle) / n
+	polyline(t, n, step_length, step_angle)
+
+def circle(t, r):
+	arc(t, r, 360)
+
+def petal(t, r, angle):
+    for i in range(2):
+   		arc(t, r, angle)
+        lt(t, 180-angle)
+
+def flower(t, n, r, angle):
+	for i in range(n):
+		petal(t, r, angle)
+		lt(t, 360.0/n)
+
+def move(t, length):
+    pu(t)
+    fd(t, length)
+    pd(t)
+
+
+move(bob, -100)
+flower(bob, 7, 60, 60)
+
+move(bob, 100)
+flower(bob, 10, 40, 80)
+
+move(bob, 100)
+flower(bob, 20, 200, 20)
+
+die(bob)
 
 
 
-# This is where you put code to move bob
 
 
 
 
 
 
-
-
-wait_for_user()					
+wait_for_user()
 
